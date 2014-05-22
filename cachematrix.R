a7aaa7c310e223f722a9c48eaf86ab2db0334da5
Shreyas Shenoy <shreyasshenoy91@gmail.com>
2014-05-22 12:00:50 +0530

    Added necessary comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly 
 
-## Write a short comment describing this function
+## In this code file we introduce the <<- operator which can be used to
+## assign a value to an object in an environment that is different from
+## the current environment.
+
+## Below are two functions that are used to create a special object
+## that stores a matrix and cache's its inverse.
+
+## The first function, makeCacheMatrix creates a special "matrix", which 
+## is really a list containing a function to
+##      1.set the value of the matrix
+##      2.get the value of the matrix
+##      3.set the value of the Inverse
+##      4.get the value of the Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         
@@ -26,7 +38,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above. If the inverse has already
+## been calculated (and the matrix has not changed), then the 
+## cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
